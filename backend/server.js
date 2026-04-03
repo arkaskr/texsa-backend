f416f1c435755c4459d1f32ecc478fb8088b7dc2
Arka <arkaskr24@gmail.com>
2026-04-03 17:53:05 +0530

    500

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-module.exports = app;
-
 app.use('/api/auth', authRoutes);
 app.use('/api/inventory', productRoutes);
 app.use('/api/dashboard', dashboardRoutes);
@@ -82,9 +80,13 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
-);
+if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
+  app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
+}
+
+module.exports = app;
